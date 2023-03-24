99e1328b8cef2afd6c74c74f6571da7e5eea85d5
Diego Carmona Bernal <cbdiegox@gmail.com>
2023-03-24 14:53:56 -0600

    main

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest_main', {
+      autoCreate: true
+    }),
+    ProductModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ main/src/product/product.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+
+@Controller('products')
+export class ProductController {
+  constructor(private productService: ProductService) {
+  }
+  
+  @Get()
+  async all() {
+    return this.productService.all();
+  }
+}
