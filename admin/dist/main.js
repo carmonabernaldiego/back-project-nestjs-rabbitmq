d254a3624bacc5821b1c59053c15c0ec83f128cd
Diego Carmona Bernal <cbdiegox@gmail.com>
2023-03-24 01:37:56 -0600

    changes

--- admin/dist/main.js
+++ admin/dist/main.js
@@ -5,6 +5,9 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.setGlobalPrefix('api');
+    app.enableCors({
+        origin: 'http://localhost:4200'
+    });
     await app.listen(8000);
 }
 bootstrap();
