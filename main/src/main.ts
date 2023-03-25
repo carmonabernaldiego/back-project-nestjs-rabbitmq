3b362b47fa50d4390eb7b8e837984d1e64335176
Diego Carmona Bernal <cbdiegox@gmail.com>
2023-03-25 12:21:51 -0600

    RabbitMQ add projects

--- main/src/main.ts
+++ main/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:4200'
+  // const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api');
+  // app.enableCors({
+  //   origin: 'http://localhost:4200'
+  // });
+  // await app.listen(8001);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    
   });
-  await app.listen(8001);
 }
 bootstrap();
